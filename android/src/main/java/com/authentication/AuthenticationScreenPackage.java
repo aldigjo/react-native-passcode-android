1b58b48fe2c840b7dfdd449b30dff4a2df947fdc
Pelle Braendgaard <pelle@stakeventures.com>
2017-09-17 21:30:42 -0600

    Tweak needed by RN 0.48 https://github.com/facebook/react-native/commit/ce6fb337a146e6f261f2afb564aa19363774a7a8

--- android/src/main/java/com/authentication/AuthenticationScreenPackage.java
+++ android/src/main/java/com/authentication/AuthenticationScreenPackage.java
@@ -16,7 +16,6 @@ public class AuthenticationScreenPackage implements ReactPackage {
     return Arrays.<NativeModule>asList(new AuthenticationScreenModule(reactContext));
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
