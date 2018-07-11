8014ddef2f7f9f4bccb7649db94d6d1772cb7f76
Mircea Nistor <mirceanis@gmail.com>
2018-07-11 19:26:01 +0300

    add fallback to manual keyguard Intent creation for API < 23

--- android/src/main/java/com/authentication/AuthenticationScreenModule.java
+++ android/src/main/java/com/authentication/AuthenticationScreenModule.java
@@ -1,18 +1,20 @@
 package com.authentication;
 
-import com.facebook.react.bridge.Callback;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
 
-import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ActivityEventListener;
 
-import android.content.Intent;
-import android.content.Context;
-import android.app.KeyguardManager;
-import android.app.Activity;
+import java.util.List;
 
 interface ActivityResultInterface {
   void callback(int requestCode, int resultCode, Intent data);
@@ -61,13 +63,53 @@ public class AuthenticationScreenModule extends ReactContextBaseJavaModule {
     }
   }
 
+  /**
+   *
+   * based on AOSP KeyguardManager.java for API 23+
+   *
+   * Get an intent to prompt the user to confirm credentials (pin, pattern or password)
+   * for the current user of the device. The caller is expected to launch this activity using
+   * {@link android.app.Activity#startActivityForResult(Intent, int)} and check for
+   * {@link android.app.Activity#RESULT_OK} if the user successfully completes the challenge.
+   *
+   * @return the intent for launching the activity or null if no password is required.
+   **/
+  public Intent createConfirmDeviceCredentialIntent(CharSequence title, CharSequence description) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+      return mKeyguardManager.createConfirmDeviceCredentialIntent(title, description);
+    } else {
+      Intent intent = new Intent("android.app.action.CONFIRM_DEVICE_CREDENTIAL");
+      intent.putExtra("android.app.extra.TITLE", title);
+      intent.putExtra("android.app.extra.DESCRIPTION", description);
+
+      // explicitly set the package for security
+      intent.setPackage(getSettingsPackageForIntent(intent));
+      return intent;
+    }
+  }
+
+
+  /**
+   * based on AOSP KeyguardManager.java for API 23+
+   */
+  private String getSettingsPackageForIntent(Intent intent) {
+    @SuppressLint("InlinedApi") List<ResolveInfo> resolveInfos = mReactContext.getPackageManager()
+            .queryIntentActivities(intent, PackageManager.MATCH_SYSTEM_ONLY);
+    for (int i = 0; i < resolveInfos.size(); i++) {
+      return resolveInfos.get(i).activityInfo.packageName;
+    }
+
+    return "com.android.settings";
+  }
+
+
 
   @ReactMethod
   public void show(String message, final Callback callback) {
     mActivity = getCurrentActivity();
     mCallback = callback;
 
-    Intent intent = mKeyguardManager.createConfirmDeviceCredentialIntent(null, message);
+    Intent intent = createConfirmDeviceCredentialIntent(null, message);
 
     if (intent != null) {
       mActivity.startActivityForResult(intent, 1);
